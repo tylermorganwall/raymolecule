f4e35f7888c84b786738a8275680fcf61ff3082e
Tyler <tylermw@gmail.com>
2021-05-18 18:31:57 -0400

    raymolecule 0.3.1: Fix SDF parsing for large molecule structures

--- R/read_sdf.R
+++ R/read_sdf.R
@@ -18,13 +18,23 @@ read_sdf = function(filename) {
   as.numeric = function(...) {
     suppressWarnings(base::as.numeric(...))
   }
-  con = file(filename,"rt")
+  con = file(filename,"rb")
   on.exit(close(con))
   readLines(con,n=3)
-  values = as.numeric(unlist(strsplit(trimws(readLines(con,n=1)), split = "\\s", perl=TRUE)))
-  molecules = values[1]
-  bonds = values[2]
-  moleculelist = strsplit(trimws(readLines(con,n=molecules)), split = "\\s+", perl=TRUE)
+  molecules = as.numeric(readChar(con,nchars = 3))
+  bonds = as.numeric(readChar(con,nchars = 3))
+  readLines(con,n=1)
+
+  moleculelist = list()
+  for(i in seq_len(molecules)) {
+    moleculelist[[i]] = matrix(trimws(c(readChar(con,nchars = 10),
+                                 readChar(con,nchars = 10),
+                                 readChar(con,nchars = 10),
+                                 readChar(con,nchars = 3),
+                                 readChar(con,nchars = 3))),ncol=5)
+    readLines(con,n=1)
+  }
+
   moleculedf = as.data.frame(do.call(rbind,moleculelist), stringsAsFactors = FALSE)
   moleculedf = moleculedf[,1:4]
   moleculedf= cbind(moleculedf,list(index=1:nrow(moleculedf)))
@@ -32,11 +42,14 @@ read_sdf = function(filename) {
   moleculedf[,2] = as.numeric(moleculedf[,2])
   moleculedf[,3] = as.numeric(moleculedf[,3])
   colnames(moleculedf) = c("x","y","z","type","index")
-  bondlist = strsplit(trimws(readLines(con,n=bonds)), split = "\\s+", perl=TRUE)
+  bondlist = list()
+  for(i in seq_len(bonds)) {
+    bondlist[[i]] = matrix(as.numeric(trimws(c(readChar(con,nchars = 3),
+                                        readChar(con,nchars = 3),
+                                        readChar(con,nchars = 3)))),ncol=3)
+    readLines(con,n=1)
+  }
   bonddf = as.data.frame(do.call(rbind,bondlist), stringsAsFactors = FALSE)
-  bonddf[,1] = as.numeric(bonddf[,1])
-  bonddf[,2] = as.numeric(bonddf[,2])
-  bonddf[,3] = as.numeric(bonddf[,3])
   colnames(bonddf) = c("from", "to", "number")
   return(list(atoms = moleculedf, bonds = bonddf))
 }
